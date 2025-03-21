c890b3b1e2f81b29bcfd518d60e686749e95e9c0
doblel09 <luisflorentino1999@gmail.com>
2025-03-21 16:39:07 -0400

    Setting up project

---
+++ CSIApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSIApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
