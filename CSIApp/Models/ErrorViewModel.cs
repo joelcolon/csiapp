df6222d10101631a7e3bca949151f32a73b13e78
joelcolon <joelcolon1616@gmail.com>
2025-03-31 22:25:21 -0400

    commit_react

--- CSIApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CSIApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
